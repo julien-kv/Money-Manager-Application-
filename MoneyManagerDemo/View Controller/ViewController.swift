afd60ae39b6bbc5ab3f9d537e5a3e6bd6d7082f6
julienKeyvalue <89767355+julienKeyvalue@users.noreply.github.com>
2021-10-29 09:36:48 +0530

    Onboarding completed

--- MoneyManagerDemo/View Controller/ViewController.swift
+++ MoneyManagerDemo/View Controller/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var onBoardVC: OnBoardViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        onBoardVC = OnBoardViewController()
+        view.addSubview(onBoardVC.view)
+
     }
 
-
 }
+
+
+
 
