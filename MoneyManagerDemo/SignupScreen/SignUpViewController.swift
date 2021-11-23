8870b4a2293627590ae980ec6602a40724bd346c
julienKeyvalue <89767355+julienKeyvalue@users.noreply.github.com>
2021-11-23 17:59:05 +0530

    mvvm architecture implemented

--- MoneyManagerDemo/SignupScreen/SignUpViewController.swift
+++ MoneyManagerDemo/SignupScreen/SignUpViewController.swift
@@ -24,7 +24,8 @@ class SignUpViewController: UIViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     let defaults = UserDefaults.standard
     var email:String?
-    var tempArray:[User] = []
+//var tempArray:[User] = []
+    var signupviewmodel = SignupViewmodel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +36,10 @@ class SignUpViewController: UIViewController {
         
     }
     override func viewWillAppear(_ animated: Bool) {
+        setBackground()
+    }
+    
+    func setBackground(){
         textfieldSet.gradient(view: view, BgView: BgView)
         navigationController?.setNavigationBarHidden(true, animated: true)
         textfieldSet.setuptextFieldForUserNameAndPassword(txtField: UserNameTextField, placeHolder: "Full Name")
@@ -48,8 +53,6 @@ class SignUpViewController: UIViewController {
         textfieldSet.setTextFieldImage(imgString: "envelope", txtField: EmailTextField)
         PwdTextField.isSecureTextEntry = true
         pwdConfirmTextField.isSecureTextEntry = true
-        
-        
         textfieldSet.setButton(btn: SignUpButton)
     }
     @IBAction func didTapBackButton(_ sender: Any) {
@@ -60,137 +63,27 @@ class SignUpViewController: UIViewController {
         GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
             guard error == nil else { return }
             self.activityViewIndicator.stopAnimating()
-            self.navigateToDashBoard(username: user?.profile?.email)
+            self.signupviewmodel.navigateToDashBoard(username: user?.profile?.email)
         }
     }
-   
     
-    func CreateNewUser(email:String){
-        do{
-        tempArray = try context.fetch(User.fetchRequest())
-        }catch{
-        }
-        if tempArray .firstIndex(where: {$0.usename == email}) != nil {
-           // do something with foo
-            return
-        } else {
-           // item could not be found
-            let newPerson = User(context: self.context)
-            newPerson.usename = email
-            do{
-                try self.context.save()
-            }catch{
-                
-            }
-        }
-    }
-    func navigateToDashBoard(username:String? = nil){
-        let storyboard = UIStoryboard(name: "MoneyManagerDashboard", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "tabbarcontroller") as! DashboardTabbarVC
-        defaults.set(username, forKey: "username")
-        self.CreateNewUser(email: username!)
-        
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(vc)
-        
-        
-    }
-    func areTextFieldsEmpty()->Bool{
-        if(UserNameTextField.text==""  || EmailTextField.text == "" || PwdTextField.text == "" || pwdConfirmTextField.text == ""){
-            return true
-        }
-        else{
-            return false
-        }
-    }
-    func showEmptyTextfieldAlert(){
-        let alert = UIAlertController(title: "Input Fields can't be empty!!",message: "ALl the fields must be filled",preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Done", style: .cancel))
-        self.present(alert, animated: true)
-    }
+ 
     @IBAction func didTapFacebookLogin(_ sender: Any) {
-        if (areTextFieldsEmpty()){
-            showEmptyTextfieldAlert()
+        if (signupviewmodel.areTextFieldsEmpty(UserNameTextField: UserNameTextField, EmailTextField: EmailTextField, PwdTextField: PwdTextField, pwdConfirmTextField: pwdConfirmTextField)){
+            
+            signupviewmodel.showEmptyTextfieldAlert(vc: self)
             return
         }
         else{
-            activityViewIndicator.startAnimating()
-            let loginManager = LoginManager()
-            loginManager.logIn(permissions: ["public_profile", "email"], from: self) { [weak self] (result, error) in
-                // Check for error
-                guard error == nil else {
-                    // Error occurred
-                    print(error!.localizedDescription)
-                    return
-                }
-                
-                // Check for cancel
-                guard let result = result, !result.isCancelled else {
-                    print("User cancelled login")
-                    self?.activityViewIndicator.stopAnimating()
-                    return
-                }
-                // Check the result
-                guard let accessToken = FBSDKLoginKit.AccessToken.current else { return }
-                let graphRequest = FBSDKLoginKit.GraphRequest(graphPath: "me",
-                                                              parameters: ["fields": "email, name"],
-                                                              tokenString: accessToken.tokenString,
-                                                              version: nil,
-                                                              httpMethod: .get)
-                graphRequest.start { (connection, result, error) -> Void in
-                    if error == nil {
-                        guard let json = result as? NSDictionary else{
-                            return
-                        }
-                        if let email=json["email"] as? String{
-                            print(email)
-                            self?.activityViewIndicator.stopAnimating()
-                            
-                            self?.navigateToDashBoard(username: email)
-                        }
-                    }
-                    else {
-                        
-                    }
-                }
-            }
+            signupviewmodel.setupFbLogin(vc: self)
         }
         
        
     }
     @IBAction func didTapSignUp(_ sender: Any) {
-        navigateToDashBoard(username: EmailTextField.text)
+        self.signupviewmodel.navigateToDashBoard(username: EmailTextField.text)
     }
-    func addUser()-> NSManagedObject?{
-        guard let appDelegate =
-                UIApplication.shared.delegate as? AppDelegate else {
-                    return nil
-                }
-        
-        // 1
-        let managedContext =
-        appDelegate.persistentContainer.viewContext
-        
-        // 2
-        let entity =
-        NSEntityDescription.entity(forEntityName: "User",
-                                   in: managedContext)!
-        
-        let person = NSManagedObject(entity: entity,
-                                     insertInto: managedContext)
-        
-        // 3
-        person.setValue(EmailTextField.text, forKeyPath: "username")
-        
-        
-        // 4
-        do {
-            try managedContext.save()
-            self.userObjectArray.append(person)
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-        return person
-    }
+
     @IBAction func didTapSignIn(_ sender: Any) {
         var flag = false
         for controller in self.navigationController!.viewControllers as Array {
--- MoneyManagerDemo/SignupScreen/SignupViewmodel.swift
+++ MoneyManagerDemo/SignupScreen/SignupViewmodel.swift
@@ -11,5 +11,130 @@ import GoogleSignIn
 import FBSDKLoginKit
 import CoreData
 class SignupViewmodel{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let defaults = UserDefaults.standard
+    var email:String?
+    var tempArray:[User] = []
     
+    func CreateNewUser(email:String){
+        do{
+        tempArray = try context.fetch(User.fetchRequest())
+        }catch{
+        }
+        if tempArray .firstIndex(where: {$0.usename == email}) != nil {
+           // do something with foo
+            return
+        } else {
+           // item could not be found
+            let newPerson = User(context: self.context)
+            newPerson.usename = email
+            do{
+                try self.context.save()
+            }catch{
+                
+            }
+        }
+    }
+    func navigateToDashBoard(username:String? = nil){
+        let storyboard = UIStoryboard(name: "MoneyManagerDashboard", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "tabbarcontroller") as! DashboardTabbarVC
+        defaults.set(username, forKey: "username")
+        self.CreateNewUser(email: username!)
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(vc)
+        
+        
+    }
+    func areTextFieldsEmpty(UserNameTextField:UITextField,EmailTextField:UITextField,PwdTextField:UITextField, pwdConfirmTextField:UITextField)->Bool{
+        if(UserNameTextField.text==""  || EmailTextField.text == "" || PwdTextField.text == "" || pwdConfirmTextField.text == ""){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    func showEmptyTextfieldAlert(vc:SignUpViewController){
+        let alert = UIAlertController(title: "Input Fields can't be empty!!",message: "ALl the fields must be filled",preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Done", style: .cancel))
+        vc.present(alert, animated: true)
+    }
+    
+    func setupFbLogin(vc:SignUpViewController){
+        let activityViewIndicator = LoadingIndicator.addIndicator(view: vc.view,type: .ballClipRotateMultiple)
+        activityViewIndicator.startAnimating()
+        let loginManager = LoginManager()
+        loginManager.logIn(permissions: ["public_profile", "email"], from: vc) { [weak self] (result, error) in
+            // Check for error
+            guard error == nil else {
+                // Error occurred
+                print(error!.localizedDescription)
+                return
+            }
+            
+            // Check for cancel
+            guard let result = result, !result.isCancelled else {
+                print("User cancelled login")
+                vc.activityViewIndicator.stopAnimating()
+                return
+            }
+            // Check the result
+            guard let accessToken = FBSDKLoginKit.AccessToken.current else { return }
+            let graphRequest = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                          parameters: ["fields": "email, name"],
+                                                          tokenString: accessToken.tokenString,
+                                                          version: nil,
+                                                          httpMethod: .get)
+            graphRequest.start { (connection, result, error) -> Void in
+                if error == nil {
+                    guard let json = result as? NSDictionary else{
+                        return
+                    }
+                    if let email=json["email"] as? String{
+                        print(email)
+                        vc.activityViewIndicator.stopAnimating()
+                        
+                        self?.navigateToDashBoard(username: email)
+                    }
+                }
+                else {
+                    
+                }
+            }
+        }
+   
+    }
+    
+//    func addUser(email:String)-> NSManagedObject?{
+//        guard let appDelegate =
+//                UIApplication.shared.delegate as? AppDelegate else {
+//                    return nil
+//                }
+//
+//        // 1
+//        let managedContext =
+//        appDelegate.persistentContainer.viewContext
+//
+//        // 2
+//        let entity =
+//        NSEntityDescription.entity(forEntityName: "User",
+//                                   in: managedContext)!
+//
+//        let person = NSManagedObject(entity: entity,
+//                                     insertInto: managedContext)
+//
+//        // 3
+//        person.setValue(email, forKeyPath: "username")
+//
+//
+//        // 4
+//        do {
+//            try managedContext.save()
+//            self.userObjectArray.append(person)
+//        } catch let error as NSError {
+//            print("Could not save. \(error), \(error.userInfo)")
+//        }
+//        return person
+//    }
+    
+    //
 }
